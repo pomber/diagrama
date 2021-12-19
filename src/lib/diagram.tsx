7c261527dc2fab6c583aa8069f6f6a606970c781
Rodrigo Pombo <pombopombopombo@gmail.com>
2021-12-19 21:36:32 +0100

    Change host node color

--- src/lib/diagram.tsx
+++ src/lib/diagram.tsx
@@ -26,12 +26,21 @@ export function Diagram({ data }: DiagramProps) {
 
 function Tree({ node }: { node: RenderNode }) {
   const truncate = 1 + node.width / 10;
+  const truncatedName = node.name && node.name.slice(0, truncate);
+  const firstChar = node.name ? node.name[0] : "";
+  const isHostNode = firstChar === firstChar.toLocaleLowerCase();
   return (
-    <g>
-      <rect x={node.x} y={node.y} width={node.width} height={node.height} />
-      <text x={node.x + 10} y={node.y + 20} fill="black">
+    <g style={{ color: isHostNode ? "#228" : "#000" }}>
+      <rect
+        x={node.x}
+        y={node.y}
+        width={node.width}
+        height={node.height}
+        stroke="currentColor"
+      />
+      <text x={node.x + 10} y={node.y + 20} stroke="currentColor">
         <title>{node.name}</title>
-        {node.name.slice(0, truncate)}
+        {truncatedName}
       </text>
       {node.children.map((child) => (
         <Tree key={child.id} node={child} />
